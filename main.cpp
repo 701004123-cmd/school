bb8587d3e2e39395e591ee20af172708b2282e28
701004123-cmd <701004123@gotvdsb.ca>
2026-05-21 13:27:34 -0400

    Add function to create new accounts
    Implement addAccount function to create new accounts.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "classes.h"
+
+using namespace std;
+
+vector<int> accounts; 
+
+void addAccount(){
+    int accountNumber = accounts.size();
+    string accountName;
+    account accountNumber;
+    accountNumber.accountNumber = accountNumber;
+    cout << "ENTER YOUR NAME:" <<endl;
+    cin >> accountName;
+    accountNumber.accountName = accountName;
+}
+
+int main(){
+     
+
+
+}
